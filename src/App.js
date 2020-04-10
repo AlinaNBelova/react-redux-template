66d7008eadbc7c641b69a150ec3cb5bf026b3ce6
Alina Belova <belovaalni@gmail.com>
2020-04-10 11:16:03 -0500

    react-redux template with local storage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        Home Page
+      </>
+    )
+  }
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import redux
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+//router
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import BaseLayout from './components/layout/BaseLayout'
+import Container from './components/Container'
+import rootReducer from './reducers/rootReducer'
+
+let saveToLocalStorage = (state)=>{
+  try{
+    const serializeState = JSON.stringify(state);
+    localStorage.setItem('state', serializeState)
+    //localStorage['state']=seriaalizeState
+
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+let loadFromLocalStorage=()=>{
+  try{
+    const serializeState=localStorage.getItem('state');
+    if(serializeState===null){
+      return undefined
+    }
+    else{
+      return JSON.parse(serializeState)
+    }
+
+  }
+  catch(e){
+    console.log(e);
+    return undefined
+  }
+}
+
+const persistedState=loadFromLocalStorage()
+
+let store = createStore(rootReducer,
+  persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <BaseLayout>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/container" component={Container} />
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+  </Provider>
+  ,
+  document.getElementById('root')
+);
